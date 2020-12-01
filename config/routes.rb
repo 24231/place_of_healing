148291251732b4a8bcb592bbc95c6c331b151012
yusuke.yamada <srbcdol2@au.com.jp>
2020-12-01 02:57:58 +0000

    basic-settiong

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
  root  'static_pages#home'
 
-  # resources:users
+ get 'login',    to:'sessions#new'
+ post 'login',   to:'sessions#create'  
+ delete 'logout',to:'sessions#destroy'
+
  get 'users/:id/show', to:'users#show'
  get 'signup',         to:'users#new'
  post 'users/create',  to:'users#create'
-  
-  
+ 
   
 end

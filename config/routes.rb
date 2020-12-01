595b573063dabc4ce43e3136821f3d000ca64584
yusuke.yamada <srbcdol2@au.com.jp>
2020-12-01 02:18:04 +0000

    basic-setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
  root  'static_pages#home'
 
-  get '/signup', to:'users#new'
- 
- 
+  # resources:users
+ get 'users/:id/show', to:'users#show'
+ get 'signup',         to:'users#new'
+ post 'users/create',  to:'users#create'
+  
+  
   
 end

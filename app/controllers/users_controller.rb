f28092838c773771f1e232133fcee6bb06a585b4
yusuke.yamada <srbcdol2@au.com.jp>
2020-12-11 03:25:22 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find_by(id: params[:id])
+    @user =  User.find_by(id: params[:id])
   end
   
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録が完了しました"
+      redirect_to "/users/#{@user.id}/show"
+    else
+      render "/users/new"
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end

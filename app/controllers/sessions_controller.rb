c96196b352f6789ca9a4aef23e0d51a84f954cbc
yusuke.yamada <srbcdol2@au.com.jp>
2020-12-12 02:15:23 +0000

    Change header layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to "/users/#{session[:user_id]}/show"
+      redirect_to  "/users/#{session[:user_id]}/show"
     else
       flash.now[:danger] ='名前またはパスワードが正しくありません'
       render '/sessions/new'

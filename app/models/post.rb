2df9888b22d876a0f7e6fecf291808965ceeee2e
yusuke.yamada <srbcdol2@au.com.jp>
2020-12-16 01:48:30 +0000

    Finished post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :place, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 140 }
+  default_scope -> { order(created_at: :desc) }
 end

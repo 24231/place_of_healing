a8d97ecfa446b83623065ad707e6abd6857a652f
yusuke.yamada <srbcdol2@au.com.jp>
2020-12-19 01:51:16 +0000

    Finished basic post function and display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,30 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :update, :destroy]
   
   def show
   end
   
   def new
+    @post = Post.new
   end
   
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿されました"
+      redirect_to root_url
+    else
+      render "posts/new"
+    end
   end
   
   def destroy
   end
+
+  private
+  
+    def post_params
+      params.require(:post).permit(:place, :content)
+    end
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   
   def show
     @user =  User.find_by(id: params[:id])
+    @posts = @user.posts.paginate(page: params[:page])
   end
   
   def new
@@ -39,13 +40,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] ="ログインしてください"
-        redirect_to login_url
-      end
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       unless current_user?(@user)
@@ -53,6 +47,5 @@ class UsersController < ApplicationController
         redirect_to root_path
       end
     end
-    
 end
 
